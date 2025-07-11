195e88bed4140677ae91d67c02edc8036b89ab83
Aaditya Goel <aadityagoel@Aadityas-Laptop.local>
2025-07-11 14:33:28 +0530

    Counting Tiles most efficient and and simple solution updated

--- DynamicProgramming/counting_tiles.cpp
+++ DynamicProgramming/counting_tiles.cpp
@@ -2,59 +2,54 @@
 #include <vector>
 
 using namespace std;
-
-using ll = long long;
-
-const int maxN = 11;
-const int maxM = 1001;
-const ll MOD = 1e9 + 7;
-
 int n, m;
-ll dp[maxN][maxM][1 << maxN];
+int MOD = 1e9 + 7;
+void preCompute(int row, int mask, int currMask, vector<vector<int>> &nextMasks)
+{
+    if (row == n)
+    {
+        nextMasks[mask].push_back(currMask);
+        return;
+    }
+    if (mask & (1 << row))
+    {
+        preCompute(row + 1, mask, currMask, nextMasks);
+        return;
+    }
+    preCompute(row + 1, mask, (currMask | (1 << row)), nextMasks);
+    if (((mask & (1 << (row + 1))) == 0) && row != n - 1)
+    {
+        preCompute(row + 2, mask, currMask, nextMasks);
+    }
+    return;
+}
 
 int main()
 {
     cin >> n >> m;
-    dp[n][0][0] = 1;
+    int mask = (1 << n) - 1;
 
-    for (int col = 1; col <= m; col++)
+    vector<int> prev(mask + 1, 0);
+    vector<vector<int>> nextMasks(mask + 1, vector<int>());
+    for (int i = 0; i <= mask; i++)
     {
-        for (int mask = 0; mask < (1 << n); mask++)
+        preCompute(0, i, 0, nextMasks);
+    }
+    prev[0] = 1;
+    for (int col = m - 1; col >= 0; col--)
+    {
+        vector<int> curr(mask + 1, 0);
+        for (int i = 0; i <= mask; i++)
         {
-            dp[0][col][mask << 1] = dp[n][col - 1][mask];
-        }
-
-        for (int row = 1; row <= n; row++)
-        {
-            int x = 1 << (row - 1);
-            int y = 1 << row;
-
-            for (int mask = 0; mask < (1 << (n + 1)); mask++)
+            for (int k : nextMasks[i])
             {
-                dp[row - 1][col][mask] %= MOD;
-
-                if ((mask & x) && (mask & y))
-                {
-                    continue;
-                }
-
-                if (mask & x)
-                {
-                    dp[row][col][mask ^ x] += dp[row - 1][col][mask];
-                }
-                else if (mask & y)
-                {
-                    dp[row][col][mask ^ y] += dp[row - 1][col][mask];
-                }
-                else
-                {
-                    dp[row][col][mask ^ x] += dp[row - 1][col][mask];
-                    dp[row][col][mask ^ y] += dp[row - 1][col][mask];
-                }
+                curr[i] += prev[k];
+                curr[i] %= MOD;
             }
         }
+        prev = curr;
     }
 
-    cout << dp[n][m][0] % MOD << endl;
+    cout << prev[0] << endl;
     return 0;
 }
